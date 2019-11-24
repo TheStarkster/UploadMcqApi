d1b4d82dd0721d20532725534dff5e7daa470696
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-11-24 20:42:02 +0530

    Project init

---
+++ Models/QFormat.cs
@@ -0,0 +1,11 @@
+namespace UploadMcqApi.Models
+{
+    public class QFormat
+    {
+        public string Question { get; set; }
+        public char Opt1 { get; set; }
+        public char Opt2 { get; set; }
+        public char Opt3 { get; set; }
+        public char Opt4 { get; set; }
+    }
+}
